3742b64decdea829894d9acfa8c18e8c8089d036
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2019-11-08 14:46:02 +0100

    [TASK] Drop unused default App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App'; // import from file
 import Main from './Main';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root')); // set the content from the file in the matching .html file
 ReactDOM.render(<Main />, document.getElementById('root')); // set the content from the file in the matching .html file
 
 // If you want your app to work offline and load faster, you can change
