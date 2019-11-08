e061050867731d3a0367dbebdddbad462e88f8bd
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2019-11-08 16:31:09 +0100

    [TASK] React to onChange and directly change the products

--- src/ProductList/ProductList.jsx
+++ src/ProductList/ProductList.jsx
@@ -47,12 +47,33 @@ export default class ProductList extends React.Component {
         });
     };
 
+    changeName = (event) => {
+        this.setState({
+            products: {
+                0: {
+                    name: event.target.value,
+                    img_src: 'https://placehold.it/250',
+                    img_alt: 'Placeholder 250x250px',
+                    price: 414,
+                    category: 'CPU',
+                },
+                1: {
+                    name: 'Some othesADASDASr CPU',
+                    img_src: 'https://placehold.it/500',
+                    img_alt: 'Placeholder 500x500px',
+                    price: 500.99,
+                    category: 'GFX',
+                }
+            }
+        });
+    };
+
     render() {
         return (
             <div>
                 <h2>Available products</h2>
                 <div class="list-group product-list">
-                    <input type="text" value={this.state.products["0"].name}/>
+                    <input type="text" onChange={this.changeName} />
                     <button type="button" onClick={this.changeProduct}>Change product</button>
                     <ProductListItem data={this.state.products["0"]}/>
                     <ProductListItem data={this.state.products["1"]}/>
