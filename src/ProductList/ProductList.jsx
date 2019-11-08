19a9f1fd408208503a46a5a8dd2e2489dfe46d7c
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2019-11-08 16:13:08 +0100

    [TASK] Basic usage of events and changing content

--- src/ProductList/ProductList.jsx
+++ src/ProductList/ProductList.jsx
@@ -25,11 +25,35 @@ export default class ProductList extends React.Component {
         }
     }
 
+    changeProduct = () => {
+        alert("Button clicked");
+        this.setState({
+            products: {
+                0: {
+                    name: 'Intel Core i7dasdas-9700K',
+                    img_src: 'https://placehold.it/250',
+                    img_alt: 'Placeholder 250x250px',
+                    price: 414,
+                    category: 'CPU',
+                },
+                1: {
+                    name: 'Some othesADASDASr CPU',
+                    img_src: 'https://placehold.it/500',
+                    img_alt: 'Placeholder 500x500px',
+                    price: 500.99,
+                    category: 'GFX',
+                }
+            }
+        });
+    };
+
     render() {
         return (
             <div>
                 <h2>Available products</h2>
                 <div class="list-group product-list">
+                    <input type="text" value={this.state.products["0"].name}/>
+                    <button type="button" onClick={this.changeProduct}>Change product</button>
                     <ProductListItem data={this.state.products["0"]}/>
                     <ProductListItem data={this.state.products["1"]}/>
                 </div>
