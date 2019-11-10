576c82accd4a15e6497770a281102007f4618eb8
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2019-11-10 14:35:02 +0100

    [FEATURE] Add simple search via Google Images (CSE) API

--- src/Api/ImageApiGrid.jsx
+++ src/Api/ImageApiGrid.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const GoogleImages = require('google-images');
+const client = new GoogleImages('011706504980353428457:zgomawhlc9k', 'AIzaSyC-11-nerHKCNJCpFsLTstJtaLDPpci2e0');
+
 export default class ImageApiGrid extends React.Component {
     constructor() {
         super();
     }
 
     render() {
+        client.search("Intel").then(images => {
+            console.log(images);
+        });
         return (
             <h3>Test</h3>
         )
